84c5ec566d4d6c4bb4971fbce381957c88431873
Johan Viklund <johan.viklund@nbis.se>
2018-04-27 12:23:37 +0200

    Create course script more or less finished

--- create_course.py
+++ create_course.py
@@ -3,14 +3,23 @@ import argparse
 
 import yaml
 import sys
+import subprocess
+import re
 
 import passlib.pwd
 import passlib.hash
 
+import jinja2
 
 from cryptography.hazmat.primitives import serialization as crypto_serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.backends import default_backend as crypto_default_backend
+
+
+def render_template(file, **kwargs):
+    env = jinja2.Environment(loader = jinja2.FileSystemLoader('.'))
+    template = env.get_template(file)
+    return template.render( **kwargs )
 
 
 def create_ssh_key():
@@ -28,6 +37,7 @@ def create_ssh_key():
         crypto_serialization.PublicFormat.OpenSSH
     )
     return (public_key.decode('utf-8'), private_key.decode('utf-8'))
+
 
 def create_users(number):
     uid_start = 2000
@@ -53,18 +63,63 @@ def create_users(number):
 
     return users
 
-def main():
 
+def generate_config_file(**args):
+    with open('config.tfvars', 'w') as fh:
+        fh.write(render_template('config.tfvars.jj2', **args))
+
+
+def generate_vars_file(args, users):
+    data = {
+        "master_host": "{}-master-000".format(args.cluster_prefix),
+        "master_ip": "{{ hostvars.get(master_host)[\"ansible_host\"] }}",
+        "users": users,
+    }
+
+    vars_file = 'playbooks/group_vars/all'
+
+    with open(vars_file, 'w') as fh:
+        fh.write(yaml.dump(data, default_flow_style=False))
+
+
+def generate_users_file(users):
+    with open('users.txt', 'w') as fh:
+        for u in users:
+            fh.write("{}\t{}\n".format(u['user'], u['password']))
+
+
+def find_external_network():
+    p = subprocess.run(['./kn', 'openstack', 'network', 'list'], stdout=subprocess.PIPE)
+    for line in p.stdout.decode('utf8').split("\r\n"):
+        m = re.search(r'([a-f0-9-]+) \| (Public External IPv4 network)', line, re.IGNORECASE)
+        if m:
+            id = m.group(1)
+            name = m.group(2)
+            return (id, name)
+    return
+
+
+def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('users', metavar='U', nargs='+', type=int, help='The number of users to generate credentials for')
+    parser.add_argument('--users',            dest='node_count',       type=int, required=True, help='The number of users to generate credentials for')
+    parser.add_argument('--cluster-prefix',   dest='cluster_prefix',   type=str, default='virt-workshop')
+    parser.add_argument('--master-flavor',    dest='master_flavor',    type=str, default='ssc.small')
+    parser.add_argument('--master-disk-size', dest='master_disk_size', type=int, default=0)
+    parser.add_argument('--node-flavor',      dest='node_flavor',      type=str, default='ssc.small')
+    parser.add_argument('--node-disk-size',   dest='node_disk_size',   type=int, default=0)
 
     args = parser.parse_args()
 
-    nusers = args.users[0]
-    users = create_users(nusers)
+    users = create_users(args.node_count)
+    (id,name) = find_external_network()
 
+    generate_config_file(**vars(args), external_network_id=id, external_network_name=name)
+    generate_vars_file(args, users)
+    generate_users_file(users)
+    print("""Course setup is finished
+ To spin up the cloud run: ./kn apply
+ The usernames and passwords are in the file users.txt""")
 
-    print(yaml.dump({"users": users}, default_flow_style=False))
 
 if __name__ == '__main__':
     main()
